1f1162b57d4022c7f2c965134c84a366d0efcc9a
lawrenceyim <lawrence.yim.dev@gmail.com>
2025-12-28 13:54:03 -0500

    Added basic player placeholder with sideway movements using A/D and arrow keys.

--- Scripts/Repository/Impl/PackedSceneRepository.cs
+++ Scripts/Repository/Impl/PackedSceneRepository.cs
@@ -3,14 +3,14 @@ using Godot.Collections;
 using RepositorySystem;
 
 public partial class PackedSceneRepository : Node, IAutoload, IRepository {
-    public static string AutoloadPath => "/root/PackedSceneRepository";
+	public static string AutoloadPath => "/root/PackedSceneRepository";
 
-    [Export]
-    private Dictionary<PackedSceneId, PackedScene> _packedScenes;
+	[Export]
+	private Dictionary<PackedSceneId, PackedScene> _packedScenes;
 
-    public PackedScene GetPackedScene(PackedSceneId packedSceneId) {
-        return _packedScenes[packedSceneId];
-    }
+	public PackedScene GetPackedScene(PackedSceneId packedSceneId) {
+		return _packedScenes[packedSceneId];
+	}
 }
 
 public enum PackedSceneId {
