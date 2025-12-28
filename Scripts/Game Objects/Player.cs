2de97dd19a3287ff2c03aca21cbb06121655c909
lawrenceyim <lawrence.yim.dev@gmail.com>
2025-12-28 14:02:41 -0500

    Added player walking and idle animation with horizontal sprite flipping for direction.

--- Scripts/Game Objects/Player.cs
+++ Scripts/Game Objects/Player.cs
@@ -4,6 +4,10 @@ using InputSystem;
 using ServiceSystem;
 
 public partial class Player : AnimatedSprite2D, IInputState, ITick {
+    private const string AttackOne = "Attack One";
+    private const string Move = "Move";
+    private const string Idle = "Idle";
+
     private GameClock _gameClock;
     private float _movementSpeed = 180 / Engine.PhysicsTicksPerSecond;
 
@@ -28,7 +32,14 @@ public partial class Player : AnimatedSprite2D, IInputState, ITick {
         if (Input.IsActionPressed("MoveRight")) {
             movementX += 1;
         }
-        
+
+        if (movementX == 0) {
+            Play(Idle);
+            return;
+        }
+
+        Play(Move);
+        FlipH = movementX < 0;
         Position += new Vector2(movementX * _movementSpeed, 0);
     }
 }
