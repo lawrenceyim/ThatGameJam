c24d85f6e1c0f0575e81eac232cb690618ac1eb1
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-01-04 14:53:59 -0500

    Final version.

--- Scripts/Scenes/MainMenu.cs
+++ Scripts/Scenes/MainMenu.cs
@@ -3,15 +3,14 @@ using System;
 using ServiceSystem;
 
 public partial class MainMenu : Node2D {
-    [Export]
-    private Button _playButton;
+	[Export]
+	private Button _playButton;
 
-    private SceneManager _sceneManager;
+	private SceneManager _sceneManager;
 
-    public override void _Ready() {
-        ServiceLocator serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
-        _sceneManager = serviceLocator.GetService<SceneManager>(ServiceName.SceneManager);
-        _playButton.Pressed += () => _sceneManager.ChangeScene(SceneId.CutScene);
-        GlobalSettings.SecretAttackUnlocked = false;
-    }
+	public override void _Ready() {
+		ServiceLocator serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
+		_sceneManager = serviceLocator.GetService<SceneManager>(ServiceName.SceneManager);
+		_playButton.Pressed += () => _sceneManager.ChangeScene(SceneId.CutScene);
+	}
 }
